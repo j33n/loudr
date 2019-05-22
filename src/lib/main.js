c8c2c6979c69976c9cc0bbddb33ccb4b706b3ace
Jean Abayo <abagammy@gmail.com>
2019-05-22 10:36:10 +0300

    project setup

--- src/lib/main.js
+++
@@ -1,4 +0,0 @@
-import Loudr from './loudr.js';
-
-var body = document.querySelector('body');
-body.textContent = 'Good point: ' + new Loudr(23, 23);
--- webpack.config.js
+++ webpack.config.js
@@ -3,26 +3,31 @@ const webpack = require('webpack');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const uglifyJsPlugin = require('uglifyjs-webpack-plugin');
 module.exports = {
-	entry: './src/lib/main.js',
-	output: {
-		library: 'loudr',
-		libraryTarget: 'umd',
-		libraryExport: 'default',
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'index.js'
-	},
 	module: {
 		rules: [{
-				test: /\.js$/,
+				test: /\.(js|jsx)$/,
 				exclude: /node_modules/,
-				use: ['babel-loader']
+				use: {
+					loader: "babel-loader"
+				}
+			},
+			{
+				test: /\.html$/,
+				use: [{
+					loader: "html-loader"
+				}]
+			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"]
 			}
 		]
 	},
 	plugins: [
 		new uglifyJsPlugin(),
 		new HTMLWebpackPlugin({
-			template: path.resolve(__dirname, 'src/index.html')
+			template: path.resolve(__dirname, 'src/index.html'),
+			filename: path.resolve(__dirname, './index.html')
 		}),
 		new webpack.HotModuleReplacementPlugin(),
 	]
